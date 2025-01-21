7854616ba1dbcbedbcbfb3a717e79bb32d7a6a4f
Louis Githogori Mariara <louismariara@gmail.com>
2025-01-21 17:23:17 +0300

    routing practise

--- server/app.py
+++ server/app.py
@@ -3,6 +3,39 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print (parameter)
+    return parameter
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    output = ''
+    for i in range (parameter):
+        print(i)
+        output += str(i) + '\n'
+    return output
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1,operation,num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Error: Invalid operation"
+    return str(result)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
